c50d7e79bebd1ec64c1441e442ec329ccc8473c3
Adryan <agzzin06@gmail.com>
2025-06-08 10:17:33 -0300

    Screen SignUp

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from '../screens/WelcomeScreen'
 import EmailLoginScreen from '../screens/EmailLoginScreen';
 import RNBootSplash from "react-native-bootsplash";
+import SignUpScreen from '../screens/SignUpScreen';
 import { useEffect } from "react";
 
 
@@ -15,9 +16,10 @@ const Routes = () => {
   }, []);
 
   return (
-    <Stack.Navigator initialRouteName="EmailLoginScreen" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName="SignUpScreen" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
       <Stack.Screen name='EmailLoginScreen' component={EmailLoginScreen} />
+      <Stack.Screen name='SignUpScreen' component={SignUpScreen} />
     </Stack.Navigator>
   );
 };
